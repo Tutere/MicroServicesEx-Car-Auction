8adfceb54c457b2a84b1c8d22000a319b4a9069e
tuteredurie <tuteredurie@hotmail.com>
2024-08-01 17:41:13 +1200

    feat: added contracts/events that will be getting sent to the service bus

---
+++ src/Contracts/AuctionCreated.cs
@@ -0,0 +1,21 @@
+﻿namespace Contracts;
+
+public class AuctionCreated
+{ //note cant just use Auction DTO because not in the same namepsace which is a requirement of mass transit
+    public Guid Id { get; set; }
+    public int ReservePrice { get; set; }
+    public String Seller { get; set; }
+    public string Winner { get; set; }
+    public int SoldAmount { get; set; }
+    public int CurrentHighBid { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public DateTime AuctionEnd { get; set; }
+    public string Status { get; set; }
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public string Color { get; set; }
+    public int Mileage { get; set; }
+    public string ImageUrl { get; set; }
+}
